ed9b0f38661f347a2e43676172b663ff83481425
Matheus Ferreira <mattferreira.dev@protonmail.com>
2024-04-13 23:02:41 -0300

    reorganize heading component

---
+++ src/components/heading/components/heading.tsx
@@ -0,0 +1,28 @@
+import cs from '~/utils/cs';
+import type { HeadingProps } from '../helpers/types';
+import { SIZES } from '../constants/styles';
+
+function Heading({
+  as: Tag = 'h2',
+  children,
+  color,
+  size = 'size1'
+}: HeadingProps) {
+  return (
+    <Tag className={cs(
+      'font-bold text-[var(--text-color)] dark:text-[var(--dark-text-color)]',
+      SIZES[size],
+    )}
+      style={{
+        // TODO: finish implementation
+        // @ts-expect-error: Caused due polymorphism
+        '--dark-heading-color': color.dark,
+        '--heading-color': color.light
+      }}
+    >
+      {children}
+    </Tag>
+  )
+}
+
+export default Heading;
---
+++ src/components/heading/constants/styles.ts
@@ -0,0 +1,11 @@
+import type { StylesOf } from '~/@types/helpers';
+import type { Sizes } from '../helpers/types';
+
+export const SIZES: StylesOf<Sizes> = {
+  size1: 'text-5xl',
+  size2: 'text-4xl',
+  size3: 'text-3xl',
+  size4: 'text-2xl',
+  size5: 'text-xl',
+  size6: 'text-lg',
+}
